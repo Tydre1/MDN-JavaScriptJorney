4359e12fea09912d18d9cd066dc2043bb798aeee
André Barbosa <oliveiraandre945@gmail.com>
2024-01-16 08:12:57 -0300

    up

---
+++ 3 - Looping Code/4 - while and do...while/2-do_while-loop.js
@@ -0,0 +1,26 @@
+// "do while" way
+initializer
+do {
+  // code to run
+
+  final-expression
+} while (condition)
+
+
+const cats = ["Pete", "Biggles", "Jasmine"];
+
+let myFavoriteCats = "My cats are called ";
+
+let i = 0;
+
+do {
+  if (i === cats.length - 1) {
+    myFavoriteCats += `and ${cats[i]}.`;
+  } else {
+    myFavoriteCats += `${cats[i]}, `;
+  }
+
+  i++;
+} while (i < cats.length);
+
+console.log(myFavoriteCats); // "My cats are called Pete, Biggles, and Jasmine."
